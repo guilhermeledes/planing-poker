d9988d0f44f569b577d63455698e56ff4f02255c
Guilherme Ledes <ledes.guilherme@gmail.com>
2020-03-24 22:06:49 -0300

    Adição do font-awesome para novos estilos na carta

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const sockets = socketio(server);
 
 app.use(express.static('public'));
+app.use(express.static('node_modules/font-awesome'));
 
 const game = createGame();
 
